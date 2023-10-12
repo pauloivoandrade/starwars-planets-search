0ede8bcd7cd3e2c78e7a27313140b22fe9d28b63
pauloivoandrade <pauloivoasantos@gmail.com>
2023-10-12 11:21:33 -0300

    Feat: filter quesito 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import useFetchApi from './Hooks/useApi';
 
 function App() {
+  const [filter, setFilter] = useState('');
   const { fetchData, loading, apiData } = useFetchApi('https://swapi.dev/api/planets');
-  // const chaves = (apiData.results).map((chave) => {
-  //   return Object.keys(chave);
-  // });
+
+  function handleSearch(e) {
+    const filterValue = e.target.value;
+    setFilter(filterValue);
+  }
+
+  const filteredPlanets = apiData.results && apiData.results
+    .filter((planet) => planet.name.includes(filter))
+    .map((planet, index) => (
+      <tr key={ index }>
+        <td>{planet.name}</td>
+        <td>{planet.rotation_period}</td>
+        <td>{planet.orbital_period}</td>
+        <td>{planet.diameter}</td>
+        <td>{planet.climate}</td>
+        <td>{planet.gravity}</td>
+        <td>{planet.terrain}</td>
+        <td>{planet.surface_water}</td>
+        <td>{planet.population}</td>
+        <td>{planet.films}</td>
+        <td>{planet.created}</td>
+        <td>{planet.edited}</td>
+        <td>{planet.url}</td>
+      </tr>
+    ));
+
   if (loading) {
     return <h2>LOADING...</h2>;
   }
+
   return (
     <div>
+      <input
+        type="text"
+        value={ filter }
+        data-testid="name-filter"
+        onChange={ (e) => handleSearch(e) }
+      />
       {/* {apiData.results?.map((result, index) => (
         <div key={ index }>
           {Object.keys(result).map((key) => (
@@ -45,23 +76,7 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {apiData.results && apiData.results.map((planet, index) => (
-            <tr key={ index }>
-              <td>{planet.name}</td>
-              <td>{planet.rotation_period}</td>
-              <td>{planet.orbital_period}</td>
-              <td>{planet.diameter}</td>
-              <td>{planet.climate}</td>
-              <td>{planet.gravity}</td>
-              <td>{planet.terrain}</td>
-              <td>{planet.surface_water}</td>
-              <td>{planet.population}</td>
-              <td>{planet.films}</td>
-              <td>{planet.created}</td>
-              <td>{planet.edited}</td>
-              <td>{planet.url}</td>
-            </tr>
-          ))}
+          { filteredPlanets}
         </tbody>
       </table>
 

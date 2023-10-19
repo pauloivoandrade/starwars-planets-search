f59ee6e93abfcd3c17bbe5ec33441001d71c5731
pauloivoandrade <pauloivoasantos@gmail.com>
2023-10-19 14:12:09 -0300

    lint lint

--- src/Hooks/useOrder.tsx
+++ src/Hooks/useOrder.tsx
@@ -1,40 +1,39 @@
-import { useState, useMemo } from 'react';
+// import { useState, useMemo } from 'react';
 
-export default function useOrder(data) {
-  const [columnSelected, setColumnSelected] = useState('population');
-  const [asc, setAsc] = useState(true);
-  const [desc, setDesc] = useState(false);
+// export default function useOrder() {
+//   const [columnSelected, setColumnSelected] = useState('population');
+//   const [asc, setAsc] = useState(true);
+//   const [desc, setDesc] = useState(false);
 
-  const sortedData = useMemo(() => {
-    function sortByKey(data, key, order = 'asc') {
-      if (order === 'asc') {
-        return data.slice().sort((a, b) => (a[key] > b[key] ? 1 : -1));
-      } else if (order === 'desc') {
-        return data.slice().sort((a, b) => (a[key] > b[key] ? -1 : 1));
-      } else {
-        throw new Error('Invalid order parameter. Please use "asc" or "desc".');
-      }
-    }
+//   const sortedData = useMemo(() => {
+//     function sortByKey(data, key, order = 'asc') {
+//       if (order === 'asc') {
+//         return data.slice().sort((a, b) => (a[key] > b[key] ? 1 : -1));
+//       } if (order === 'desc') {
+//         return data.slice().sort((a, b) => (a[key] > b[key] ? -1 : 1));
+//       }
+//       throw new Error('Invalid order parameter. Please use "asc" or "desc".');
+//     }
 
-    return sortByKey(data, columnSelected, asc ? 'asc' : 'desc');
-  }, [data, columnSelected, asc]);
+//     return sortByKey(data, columnSelected, asc ? 'asc' : 'desc');
+//   }, [data, columnSelected, asc]);
 
-  function handleSort(column) {
-    if (column === columnSelected) {
-      setAsc(!asc);
-      setDesc(!desc);
-    } else {
-      setColumnSelected(column);
-      setAsc(true);
-      setDesc(false);
-    }
-  }
+//   function handleSort(column) {
+//     if (column === columnSelected) {
+//       setAsc(!asc);
+//       setDesc(!desc);
+//     } else {
+//       setColumnSelected(column);
+//       setAsc(true);
+//       setDesc(false);
+//     }
+//   }
 
-  return {
-    sortedData,
-    columnSelected,
-    asc,
-    desc,
-    handleSort,
-  };
-}
+//   return {
+//     sortedData,
+//     columnSelected,
+//     asc,
+//     desc,
+//     handleSort,
+//   };
+// }
